674a4e0f2b2cec5670a8919443ab772bc7e3a99a
Abhiram <abhiram.builds@gmail.com>
2025-09-04 03:49:37 +0530

    Update 0007-reverse-integer.java

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,22 +1,13 @@
 class Solution {
     public int reverse(int x) {
-        int rev = 0; // Stores the reversed number
-        
+        int rev = 0;
         while (x != 0) {
-            int pop = x % 10;  // Get the last digit
-            x /= 10;           // Remove last digit from x
-
-            // Check for overflow BEFORE actually multiplying/reversing
-            if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) {
-                return 0; // Overflow for positive
-            }
-            if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) {
-                return 0; // Overflow for negative
-            }
-
-            rev = rev * 10 + pop; // Add the digit to reversed number
+            int digit = x % 10;
+            x /= 10;
+            if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && digit > 7)) return 0;
+            if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && digit < -8)) return 0;
+            rev = rev * 10 + digit;
         }
-        
         return rev;
     }
 }
