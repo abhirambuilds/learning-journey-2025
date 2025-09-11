ee266bbe9e25d9da679195538263d9b74c25a50c
Abhiram <abhiram.builds@gmail.com>
2025-09-11 09:56:12 +0530

    Delete 3516-find-closest-person directory

--- 3516-find-closest-person/3516-find-closest-person.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int findClosest(int x, int y, int z) {
-       int xz=Math.abs(x-z);
-       int yz=Math.abs(y-z);
-       if(xz==yz)
-        return 0;
-        return xz>yz ? 2:1;
-    }
-}
